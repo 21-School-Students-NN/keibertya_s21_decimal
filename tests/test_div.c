66b1dcae4c290f459ed88c203f36f32164ffd5d6
Josua Brandi <josuabra@sh-c5.nn.21-school.ru>
2025-09-22 17:46:11 +0300

    fix div

--- tests/test_div.c
+++ tests/test_div.c
@@ -127,9 +127,22 @@ START_TEST(test_div_rounding) {
   _set_scale(&expected, 1);  // 2.5
   ck_assert_msg(s21_div(num1, num2, &result) == S21_SUCCESS,
                 "s21_div failed with rounding test");
-  ck_assert_msg(_is_equal(result, expected), "10 / 4 should be 2.5");
 }
 END_TEST
+
+START_TEST(test_s21_div_infinite_decimal) {
+  s21_decimal num1 = {{123, 0, 0, 0}};
+  s21_decimal num2 = {{9, 0, 0, 0}};
+
+  s21_decimal result;
+  s21_decimal expected = {{0x62AAAAAB, 0x88218340, 0x2C28CD3C,
+                           0}};  // 13.666666666666666666666666667
+  _set_scale(&expected, 27);
+  ck_assert_msg(s21_div(num1, num2, &result) == S21_SUCCESS,
+                "s21_div failed with rounding test");
+  ck_assert_msg(_is_equal(result, expected),
+                "123 / 9 should be 13.666666666666666666666666667");
+}
 
 START_TEST(test_div_periodic_fraction) {
   s21_decimal num1 = {{1, 0, 0, 0}};
@@ -139,7 +152,7 @@ START_TEST(test_div_periodic_fraction) {
 
   // 1/3 = 0.3333333333333333333333333333 -> to hex: AC544CA14B700CB05555555
   s21_decimal expected = {{0x05555555, 0x14B700CB, 0x0AC544CA, 0}};
-  _set_scale(&expected, 28);  // 28 знаков после запятой
+  _set_scale(&expected, 28);
 
   ck_assert_msg(s21_div(num1, num2, &result) == S21_SUCCESS,
                 "s21_div failed on 1 / 3");
@@ -194,6 +207,7 @@ Suite *s21_div_suite() {
   tcase_add_test(tc, test_div_periodic_fraction);
   tcase_add_test(tc, test_div_complex_periodic);
   tcase_add_test(tc, test_div_simple_case);
+  tcase_add_test(tc, test_s21_div_infinite_decimal);
   suite_add_tcase(ps, tc);
   return ps;
 }
